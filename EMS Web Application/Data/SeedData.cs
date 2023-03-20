ac7b69e6c8cebc34ac746c5a9d45812a3da5ff08
zenitsu-agatsuma12 <root.alvin19@gmail.com>
2023-03-20 09:15:45 +0800

    For Admin

--- EMS Web Application/Data/SeedData.cs
+++ EMS Web Application/Data/SeedData.cs
@@ -8,15 +8,11 @@ namespace EMS_Web_Application.Data
         public static void SeedDefaultData(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Employee>().HasData(
-                   new Employee(1, "Alvin Root", DateTime.Now.AddDays(1), "alvin@gmail.com", "099232312",  1),
-                   new Employee(2, "Tricia Tagle", DateTime.Now.AddDays(2), "trish@gmail.com", "099232322312",  2),
-                   new Employee(3, "Joan DC", DateTime.Now.AddDays(3), "Joan@gmail.com", "09319232312", 3)
+                   new Employee(1, "Alvin Root", DateTime.Today.AddDays(1), "alvin@gmail.com", "099232312",  1)
                    );
 
             modelBuilder.Entity<Department>().HasData(
-                  new Department(1, "IT"),
-                  new Department(2, "HR"),
-                  new Department(3, "CSR")
+                  new Department(1, "Admin")
                   );
         }   
     }
